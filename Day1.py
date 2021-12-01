b4935b6efce037d3607169eb2334b3a5c8dbdf3b
Caulfield, Aideen <aideen.caulfield@optum.com>
2021-12-01 14:37:10 +0000

    Adding docstrings

--- Day1.py
+++ Day1.py
@@ -2,12 +2,11 @@ import time
 
 
 def parse(file_loc):
-    '''
+    """
     read input to list
-
     :param file_loc: file path to AOC Day1_input.txt
-    :return list_containers: list of container sizes
-    '''
+    :return floor_depth: list of depths recorded on submarine
+    """
     floor_depth = []
     with open(file_loc, "r") as myfile:
         for line in myfile:
@@ -17,27 +16,33 @@ def parse(file_loc):
 
 
 def depth_increase(floor_depth):
-    count = 0
+    """
+    checks if the depth has increased and counts how many times it does so
+    :param floor_depth: a list of depths recorded
+    :return: count: int representing how many times the depth increased
+    """
+
+    depth_count = 0
     for i in range(1, len(floor_depth)):
         if floor_depth[i] > floor_depth[i - 1]:
-            count = count + 1
+            depth_count = depth_count + 1
 
-    return count
+    return depth_count
 
 
 def main(file):
     floor_depth = parse(file)
-    count = depth_increase(floor_depth)
+    depth_count = depth_increase(floor_depth)
 
-    return count
+    return depth_count
 
 
 if __name__ == '__main__':
     start_time = time.time()
     file_loc = 'Day1_input.txt'
 
-    count = main(file_loc)
-    print(f'The depth increases {count} times')
+    depth_increase_count = main(file_loc)
+    print(f'The depth increases {depth_increase_count} times')
 
     end_time = time.time()
     print(f'Time taken:{end_time - start_time}')
